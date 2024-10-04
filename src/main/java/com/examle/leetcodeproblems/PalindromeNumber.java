ebc8d4e3a008d4ba69902a7ae34dd9039fc5e462
Vato Tsitskishvili <vatotsitskishvili1@gmail.com>
2024-10-04 17:13:02 +0400

    added Palindrome Number solution

---
+++ src/main/java/com/examle/leetcodeproblems/PalindromeNumber.java
@@ -0,0 +1,21 @@
+package com.examle.leetcodeproblems;
+
+/*
+number: 9
+difficulty: Easy
+*/
+
+public class PalindromeNumber {
+    public static boolean isPalindrome(int x) {
+        int sum = 0;
+        int target = x;
+
+        while (x > 0) {
+            int temp = x % 10;
+            x = x / 10;
+            sum = sum * 10 + temp;
+        }
+
+        return sum == target;
+    }
+}
